82f8b09d446ecc4f5178356728ac410b497ccbc6
April <macuser@Mac-User.local>
2020-07-28 17:12:23 -0600

    make footer sticky

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+
 import './App.css';
+// import logo from './logo.svg';
+import github from './GitHub_Logo.png';
+import linkedin from './LI-Logo.png';
+
+import NavBar from '../src/components/NavBar/NavBar';
+
+function openLTab() {
+  window.open('https://www.linkedin.com/in/aprilkrg/');
+}
+
+function openGTab() {
+  window.open('https://github.com/aprilkrgonzales/');
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
+      <body>
+        <p>APRIL K R GONZALES</p>
+      </body>
+      <footer className='Footer'>
+        <p>aprilkrgonzales@gmail.com</p>
+        <p>Denver, CO</p>
+        <div classname='FooterLogo'>
+          <Link onClick={openLTab}>
+            <img src={linkedin} style={{width:'20vh'}} alt='LinkedIn Logo'></img>
+          </Link>
+          <Link onClick={openGTab} to={{ pathname: 'https://github.com/aprilkrgonzales/' }}>
+            <img src={github} style={{width:'20vh'}} alt='GitHub Logo'></img>
+          </Link>
+        </div>
+        <p classname='FooterCopyright'>© 2020 April Gonzales</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component {
+    render() {
+        return(
+            <>
+                <div className='NavBar'>
+                    <button>&#x2630;</button>
+
+                </div>
+            </>
+        )
+    }
+}
+
+export default NavBar;
